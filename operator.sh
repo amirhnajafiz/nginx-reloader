3ee035d4978d0cd002bcde65ceb187e6c0ee6699
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-14 10:35:24 +0330

    add: deployment file

--- operator.sh
+++ operator.sh
@@ -19,30 +19,7 @@ kubectl get --watch --output-watch-events configmap \
 
         case $EVENT in
             ADDED|MODIFIED)
-                kubectl apply -f - << EOF
-apiVersion: apps/v1
-kind: Deployment
-metadata: { name: $NAME }
-spec:
-    selector:
-        matchLabels: { app: $NAME }
-    template:
-        metadata:
-            labels: { app: $NAME }
-            annotations: { kubectl.kubernetes.io/restartedAt: $(date) }
-        spec:
-            containers:
-            - image: nginx:1.7.9
-              name: $NAME
-              ports:
-              - containerPort: 80
-              volumeMounts:
-              - { name: data, mountPath: /usr/share/nginx/html )
-            volumes:
-            - name: data
-              configMap:
-                name: $NAME
-EOF
+                kubectl apply -f deployment.yml
                 ;;
             DELETED)
                 kubectl delete deploy $NAME
