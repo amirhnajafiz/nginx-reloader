d4496769d54e39b7fee171ad1f3d742b6a2af57c
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-02 11:14:55 +0330

    feat: reconciler

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,1 +1,17 @@
 package controller
+
+import (
+	"k8s.io/api/apps/v1beta1"
+	v1 "k8s.io/api/core/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&v1.ConfigMap{}).
+		Owns(&v1beta1.Deployment{}).
+		WithEventFilter(predicate.GenerationChangedPredicate{}).
+		Complete(r)
+}
--- internal/controller/handler.go
+++ internal/controller/handler.go
@@ -1,9 +1,14 @@
 package controller
 
 import (
+	"context"
+
 	"github.com/go-logr/logr"
+	"github.com/opdev/subreconciler"
 	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
@@ -20,4 +25,12 @@ func NewReconciler(mgr manager.Manager) *Reconciler {
 		Client: mgr.GetClient(),
 		scheme: mgr.GetScheme(),
 	}
+}
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	return subreconciler.Evaluate(subreconciler.DoNotRequeue())
 }
