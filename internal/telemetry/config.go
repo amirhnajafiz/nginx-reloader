ea9b2f6a66d4c0da2f13496e3cf7fcec07b1b294
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-03 10:50:51 +0330

    init: telemetry config

--- internal/telemetry/config.go
+++ internal/telemetry/config.go
@@ -1,1 +1,11 @@
 package telemetry
+
+import (
+	"github.com/amirhnajafiz/nginx-configmap-operator/internal/telemetry/metrics"
+	"github.com/amirhnajafiz/nginx-configmap-operator/internal/telemetry/trace"
+)
+
+type Config struct {
+	Metrics metrics.Config
+	Tracing trace.Config
+}
