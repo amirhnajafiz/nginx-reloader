fe15a3511b41e5bd9ab8361bd23c755d68007b99
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-04 12:45:30 +0330

    add: metrics server

--- internal/telemetry/metrics/server.go
+++ internal/telemetry/metrics/server.go
@@ -1,1 +1,19 @@
 package metrics
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func Server(port int) {
+	mux := http.NewServeMux()
+
+	mux.Handle("/metrics", promhttp.Handler())
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != nil {
+		log.Println(fmt.Errorf("failed to start promhttp: %w", err))
+	}
+}
