0313cc2a398eee5d5f9e8debcea618ffeb6215c7
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-04 12:47:37 +0330

    add: metrics

--- internal/telemetry/metrics/metrics.go
+++ internal/telemetry/metrics/metrics.go
@@ -1,8 +1,12 @@
 package metrics
 
+import "github.com/prometheus/client_golang/prometheus"
+
 type Metrics struct {
 	// Number of NginX deployments per namespace (gauge vector)
+	Deployments *prometheus.GaugeVec
 	// Number of NginX configmaps per namespace (gauge vector)
+	Configmaps *prometheus.GaugeVec
 	// Number of failed fetch per namespace (counter vector)
-	// Operation response time (histogram vector)
+	Failure *prometheus.CounterVec
 }
