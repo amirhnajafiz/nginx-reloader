ae7981a7b48b209079d02e9f93cded82f1529406
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-06 13:20:22 +0330

    add: config struct

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/amirhnajafiz/nginx-configmap-operator
 
 go 1.20
+
+require github.com/caarlos0/env/v11 v11.0.1 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,6 @@
 package config
+
+type Config struct {
+	Type    string `env:"NR_TYPE" envDefault:"download"`
+	Address string `env:"NR_ADDRESS,required"`
+}
