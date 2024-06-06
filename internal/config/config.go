92507aabe8184c633b0f132c74c7176424ba9fe8
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-06 13:26:47 +0330

    add: new env variables for config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,26 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
+
+// Config struct is used to store reloader parameters.
 type Config struct {
-	Type    string `env:"NR_TYPE" envDefault:"download"`
-	Address string `env:"NR_ADDRESS,required"`
+	Type         string `env:"NR_TYPE" envDefault:"download"`
+	Address      string `env:"NR_ADDRESS,required"`
+	NginxHTMLDir string `env:"NR_NGINX_HTML_DIR" envDefault:"/usr/share/nginx/html"`
+	TmpLocalDir  string `env:"NR_TMP_LOCAL_DIR" envDefault:"/etc/nginx-reloader/tmp"`
+}
+
+// Load env variables into a config struct.
+func Load() (*Config, error) {
+	instance := &Config{}
+
+	if err := env.Parse(instance); err != nil {
+		return nil, fmt.Errorf("failed to parse config values: %v", err)
+	}
+
+	return instance, nil
 }
