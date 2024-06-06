54e8ecb2b1127c7d39001776d38f22e8397acf52
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-06 13:13:21 +0330

    remove: pre codes

--- internal/config/default.go
+++ internal/config/default.go
@@ -1,12 +1,1 @@
 package config
-
-import "github.com/amirhnajafiz/nginx-configmap-operator/internal/telemetry"
-
-func Default() Config {
-	return Config{
-		MetricsBindAddress:     8080,
-		HealthProbeBindAddress: 8081,
-		LeaderElect:            false,
-		Telemetry:              telemetry.Config{},
-	}
-}
