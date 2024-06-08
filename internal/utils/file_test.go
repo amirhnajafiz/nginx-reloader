db552f3063d2ceb407355a94525493c6e9c89210
amirhnajafiz <najafizadeh21@gmail.com>
2024-06-08 12:00:07 +0330

    add: test utils

--- internal/utils/file_test.go
+++ internal/utils/file_test.go
@@ -1,24 +1,45 @@
 package utils
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
+
+func generatePath(path ...string) {
+	for _, item := range path {
+		os.Mkdir(item, 0700)
+	}
+}
 
 func generateFiles(size int, path string) {
+	dummy := "dummy message"
 
+	for i := 0; i < size; i++ {
+		address := fmt.Sprintf("%s/file.%d", path, i)
+
+		if err := os.WriteFile(address, []byte(dummy), 0700); err != nil {
+			log.Println(err.Error())
+		}
+	}
 }
 
 func checkExistance(path string) int {
-	return 0
+	files, _ := os.ReadDir(path)
+	return len(files)
 }
 
 func removeFiles(path string) {
-
+	os.RemoveAll(path)
 }
 
 func TestMoveFiles(t *testing.T) {
-	src := ""
-	dest := ""
-	size := 0
+	src := "./tmp-1"
+	dest := "./tmp-2"
+	size := 10
 
+	generatePath("./tmp-1", "./tmp-2")
 	generateFiles(size, src)
 
 	MoveDir(src, dest)
@@ -28,4 +49,5 @@ func TestMoveFiles(t *testing.T) {
 	}
 
 	removeFiles(dest)
+	removeFiles(src)
 }
